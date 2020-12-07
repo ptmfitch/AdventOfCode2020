98d2d3c44cd6b7effb3f0f1105a323277f59ef6d
Peter Fitch <peter.fitch@imanage.com>
2020-12-07 18:00:29 +0000

    day 7 complete

---
+++ day7part1.py
@@ -0,0 +1,30 @@
+import re
+
+rule_dict = {}
+with open("inputs/day7.txt") as input_file:
+    for line in input_file:
+        rule_match = re.match(r'^([a-z]+ [a-z]+) bags contain (.*?)\.$', line.rstrip())
+        parent_bag = rule_match.group(1)
+        child_bags = []
+        for child in rule_match.group(2).split(', '):
+            if child == 'no other bags':
+                break
+            child_bags.append(re.match(r'^\d+ ([a-z]+ [a-z]+) bags?$', child).group(1))
+        rule_dict[parent_bag] = child_bags
+
+res = set()
+root = 'shiny gold'
+
+
+def loop(my_dict, root_key):
+    ls = []
+    for key, val in my_dict.items():
+        if root_key in val:
+            res.add(key)
+            ls.append(key)
+    for new_root in ls:
+        loop(my_dict, new_root)
+
+
+loop(rule_dict, root)
+print(len(res))
---
+++ day7part2.py
@@ -0,0 +1,29 @@
+import re
+
+rule_dict = {}
+with open("inputs/day7.txt") as input_file:
+    for line in input_file:
+        rule_match = re.match(r'^([a-z]+ [a-z]+) bags contain (.*?)\.$', line.rstrip())
+        parent_bag = rule_match.group(1)
+        child_dict = {}
+        for child in rule_match.group(2).split(', '):
+            if child == 'no other bags':
+                break
+            child_match = re.match(r'^(\d+) ([a-z]+ [a-z]+) bags?$', child)
+            child_dict[child_match.group(2)] = int(child_match.group(1))
+        rule_dict[parent_bag] = child_dict
+
+res = set()
+root = 'shiny gold'
+
+
+def loop(my_dict, root_key):
+    stems = my_dict[root_key]
+    total = 0
+    for key, val in stems.items():
+        total += val
+        total += (loop(my_dict, key) * val)
+    return total
+
+
+print(loop(rule_dict, root))
